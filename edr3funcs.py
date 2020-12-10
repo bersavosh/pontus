2d64d04fb4c082e3baf8f82228f9916ab32c3452
Arash Bahramian <arash.bmn@gmail.com>
2020-12-10 12:05:48 +0800

    update

--- edr3funcs.py
+++ edr3funcs.py
@@ -1,10 +1,11 @@
+import os
 import numpy as np
 from scipy.integrate import quad
 from scipy.optimize import fsolve
 from scipy.interpolate import interp1d
 
 import matplotlib.pyplot as plt
-plt.rc('text', usetex=False)
+plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 colorset = ['#000000','#00270C','#00443C','#005083',
             '#034BCA','#483CFC','#9C2BFF','#EB24F4',
@@ -12,6 +13,7 @@ colorset = ['#000000','#00270C','#00443C','#005083',
             '#EBD155','#D3F187','#D7FFC8','#FFFFFF']
 
 import astropy.units as u
+from astropy.io import ascii
 from astropy.coordinates import SkyCoord
 from astropy.table import Table, Column, vstack
 from astroquery.utils.tap.core import TapPlus
@@ -19,6 +21,57 @@ from astroquery.simbad import Simbad
 
 from zero_point import zpt
 zpt.load_tables()
+
+def bailerjones(RA, DEC,parallax,parallax_er):
+    c_icrs = SkyCoord(ra=RA*u.degree, dec=DEC*u.degree, frame='icrs') # RA and DEC in degrees of the source
+    l = c_icrs.galactic.l.deg                                         # Converting to Galactic longitude 
+    b = c_icrs.galactic.b.deg                                         # Converting to Galactic latitude
+
+    rscript = f"""# Input data
+# Specify either l,b or rlen. Set other to NA. rlen takes precedence.
+w    <- {parallax} # parallax in mas (corrected for any zeropoint offset; +0.029mas in the catalogue)
+wsd  <- {parallax_er} # parallax uncertainty in mas
+glon <- {l} # Galactic longitude in degrees (0 to 360)
+glat <-  {b} # Galactic latitude (-90 to +90)
+rlen <-  NA # length scale in pc
+# Plotting parameters in pc
+# rlo,rhi are range for computing normalization of posterior
+# rplotlo, rplothi are plotting range (computed automatically if set to NA)
+rlo <- 0
+rhi <- 1e5
+rplotlo <- NA
+rplothi <- NA
+
+source("distest_single.R")
+"""
+    f = open("Gaia-DR2-distances/rscript.r", "w")
+    f.write(rscript)
+    f.close()
+    os.chdir('./Gaia-DR2-distances/')
+    os.system('r rscript.r')
+    os.chdir('..')
+    bj_post = ascii.read('./Gaia-DR2-distances/bj_post.txt', names = ['d','pdf'])
+    bj_results = ascii.read('./Gaia-DR2-distances/bj_post_res.txt',data_start=0,names=['point_est'])
+    bj_post['d'] /= 1000.0
+    bj_post['pdf'] *= 1000.0
+    bj_results['point_est'] /= 1000.0
+    return bj_post, bj_results
+
+def bailerjones_new(src_id):
+    gaia = TapPlus(url="http://dc.g-vo.org/tableinfo/")
+
+    adql_query_newbj = f"""SELECT 
+                                source_id,
+                                r_med_geo, r_lo_geo, r_hi_geo,
+                                r_med_photogeo, r_lo_photogeo, r_hi_photogeo 
+                           FROM gedr3dist.main
+                              WHERE source_id = {src_id}
+                        """
+
+    job = gaia.launch_job(adql_query_newbj)
+    result = job_dr2.get_results()
+    return result
+
 
 def atri_dist(parallax, parallax_er, ra, dec):
     # Defining some global constants which do not change with source
@@ -185,6 +238,7 @@ def atri_dist(parallax, parallax_er, ra, dec):
     posterior_func = interp1d(x,y_norm,bounds_error=False,fill_value=0.0)
     return r_mode, posterior_func, x
 
+
 def atri_CIcalc(tau, atri_MAP, atri_pdf, prefix='dr?'):
     def atrilofunc(x):
         if (x < atri_MAP) & (x > 0.1):
@@ -278,61 +332,12 @@ def gaia_zpcorr(gaia_table,drno):
     gaia_table.add_columns([newcol1,newcol2],indexes=[12,12])
     return gaia_table
 
-def comparison(name, dr2, dr3, keys = ['designation','parallax','parallax_error','parallax_over_error','zpcorr_val','parallax_zpcorr','pmra','pmra_error','pmdec','pmdec_error']):
-    c = vstack([dr2[keys],dr3[keys]])
 
-    fig = plt.figure(figsize=(8,6))
-    ax0 = fig.add_subplot(3,1,1)
-    ax0.set_title(name, fontsize=20)
-    ax0.errorbar(x = c[0]['parallax'], xerr = c[0]['parallax_error'], y = ['DR2'], 
-                 fmt='x',ms = 10,color=colorset[4],elinewidth=1.9,capsize=3,label='w/o ZP corr')
-    ax0.errorbar(x = c[1]['parallax'], xerr = c[1]['parallax_error'], y = ['DR3'], 
-                 fmt='x',ms = 10,color=colorset[9],elinewidth=1.9,capsize=3)
-    ax0.errorbar(x = c[0]['parallax_zpcorr'], xerr = c[0]['parallax_error'],y = 0.1,
-                 fmt='.',ms = 10,color=colorset[4],elinewidth=1.9,capsize=3,label='w/ ZP corr')
-    ax0.errorbar(x = c[1]['parallax_zpcorr'], xerr = c[1]['parallax_error'],y = 1.1,
-                 fmt='.',ms = 10,color=colorset[9],elinewidth=1.9,capsize=3)
-    ax0.set_xlabel('Parallax (mas)',fontsize=16)
-    ax0.set_ylim(-0.5,1.5)
-    ax0.legend(fontsize=16,bbox_to_anchor=(1,1))
-    ax0.tick_params(axis='both', which='major', labelsize=16)
-    ax0.tick_params(axis='both', which='major', length=5)
-    ax0.tick_params(axis='both', which='minor', length=2.5)
-    ax0.tick_params(axis='both', which='both',direction='in',right=True,top=True)
-    
-    
-    ax1 = fig.add_subplot(3,1,2)
-    ax1.errorbar(x = c[0]['pmra'], xerr = c[0]['pmra_error'], y = ['DR2'], 
-                 fmt='.',ms = 10,color=colorset[4],elinewidth=1.9,capsize=3)
-    ax1.errorbar(x = c[1]['pmra'], xerr = c[1]['pmra_error'], y = ['DR3'], 
-                 fmt='.',ms = 10,color=colorset[9],elinewidth=1.9,capsize=3)
-    ax1.set_xlabel(r'$\mu_\alpha$ (mas yr$^{-1}$)',fontsize=16)
-    ax1.set_ylim(-0.5,1.5)
-    ax1.tick_params(axis='both', which='major', labelsize=16)
-    ax1.tick_params(axis='both', which='major', length=5)
-    ax1.tick_params(axis='both', which='minor', length=2.5)
-    ax1.tick_params(axis='both', which='both',direction='in',right=True,top=True)
-    
-    
-    ax2 = fig.add_subplot(3,1,3)
-    ax2.errorbar(x = c[0]['pmdec'], xerr = c[0]['pmdec_error'], y = ['DR2'], 
-                 fmt='.',ms = 10,color=colorset[4],elinewidth=1.9,capsize=3)
-    ax2.errorbar(x = c[1]['pmdec'], xerr = c[1]['pmdec_error'], y = ['DR3'], 
-                 fmt='.',ms = 10,color=colorset[9],elinewidth=1.9,capsize=3)
-    ax2.set_xlabel(r'$\mu_\delta$ (mas yr$^{-1}$)',fontsize=16)
-    ax2.set_ylim(-0.5,1.5)
-    ax2.tick_params(axis='both', which='major', labelsize=16)
-    ax2.tick_params(axis='both', which='major', length=5)
-    ax2.tick_params(axis='both', which='minor', length=2.5)
-    ax2.tick_params(axis='both', which='both',direction='in',right=True,top=True)
-    fig.subplots_adjust(hspace=0.6)
-    return fig
-
-def comparison(name, dr2, dr3, 
-               atri_MAP_dr2, atri_pdf_dr2, distrange_dr2,
-               atri_MAP_dr3, atri_pdf_dr3, distrange_dr3,
-               lolim_dr2, uplim_dr2, ci_dr2,
-               lolim_dr3, uplim_dr3, ci_dr3,
+def comparison(name, dr2, dr3, mode,
+               MAP_dr2, pdf_dr2, distrange_dr2,
+               MAP_dr3, pdf_dr3, distrange_dr3,
+               lolim_dr2, uplim_dr2,
+               lolim_dr3, uplim_dr3,
                distxrange = [0.1,10],
                keys = ['designation','parallax','parallax_error','parallax_over_error','zpcorr_val','parallax_zpcorr','pmra','pmra_error','pmdec','pmdec_error']):
     
@@ -383,19 +388,26 @@ def comparison(name, dr2, dr3,
     ax2.tick_params(axis='both', which='both',direction='in',right=True,top=True)
     
     ax3 = plt.subplot2grid((3,2),(0,0),fig=fig, rowspan=3)
-    ax3.plot(distrange_dr2,atri_pdf_dr2(distrange_dr2),color=colorset[4],label='DR2')
-    ax3.axvline(atri_MAP_dr2,color=colorset[4],linestyle='--')
+    if mode == 'Atri':
+        ax3.plot(distrange_dr2,pdf_dr2(distrange_dr2),color=colorset[4],label='DR2')
+        ax3.plot(distrange_dr3,pdf_dr3(distrange_dr3),color=colorset[9],label='EDR3')
+    elif mode == 'BJ':
+        ax3.plot(distrange_dr2,pdf_dr2,color=colorset[4],label='DR2')
+        ax3.plot(distrange_dr3,pdf_dr3,color=colorset[9],label='EDR3')
+    ax3.axvline(MAP_dr2,color=colorset[4],linestyle='--')
     ax3.axvline(lolim_dr2,color=colorset[4],linestyle=':')
     ax3.axvline(uplim_dr2,color=colorset[4],linestyle=':')
-    ax3.plot(distrange_dr3,atri_pdf_dr3(distrange_dr3),color=colorset[9],label='EDR3')
-    ax3.axvline(atri_MAP_dr3,color=colorset[9],linestyle='--')
+    ax3.axvline(MAP_dr3,color=colorset[9],linestyle='--')
     ax3.axvline(lolim_dr3,color=colorset[9],linestyle=':')
     ax3.axvline(uplim_dr3,color=colorset[9],linestyle=':')
     ax3.legend(fontsize=16)
     ax3.set_xlim(distxrange)
     ax3.set_ylim(0,)
-    ax3.set_title(f'DR2: $d$={atri_MAP_dr2:.3}(-{atri_MAP_dr2-lolim_dr2[0]:.3}/+{uplim_dr2[0]-atri_MAP_dr2:.3}) kpc\nEDR3: $d$={atri_MAP_dr3:.3}(-{atri_MAP_dr3-lolim_dr3[0]:.3}/+{uplim_dr3[0]-atri_MAP_dr3:.3}) kpc', fontsize=20)
-    ax3.set_xlabel(r'Distance (kpc)',fontsize=16)
+    if mode == 'Atri':
+        ax3.set_title(f'DR2: $d$={MAP_dr2:.3}(-{MAP_dr2-lolim_dr2[0]:.3}/+{uplim_dr2[0]-MAP_dr2:.3}) kpc\nEDR3: $d$={MAP_dr3:.3}(-{MAP_dr3-lolim_dr3[0]:.3}/+{uplim_dr3[0]-MAP_dr3:.3}) kpc', fontsize=20)
+    elif mode == 'BJ':
+        ax3.set_title(f'DR2: $d$={MAP_dr2:.3}(-{MAP_dr2-lolim_dr2:.3}/+{uplim_dr2-MAP_dr2:.3}) kpc\nEDR3: $d$={MAP_dr3:.3}(-{MAP_dr3-lolim_dr3:.3}/+{uplim_dr3-MAP_dr3:.3}) kpc', fontsize=20)
+    ax3.set_xlabel(f'Distance (kpc; {mode} prior)',fontsize=16)
     ax3.set_ylabel(r'PDF (kpc$^{-1}$)',fontsize=16)
     ax3.minorticks_on()
     ax3.tick_params(axis='both', which='major', labelsize=16)
@@ -405,3 +417,162 @@ def comparison(name, dr2, dr3,
     
     fig.subplots_adjust(hspace=0.6, wspace=0.11)
     return c, fig
+
+
+def comparison_all(name, dr2, dr3, 
+                   atri_MAP_dr2, atri_pdf_dr2, distrange_dr2,
+                   atri_MAP_dr3, atri_pdf_dr3, distrange_dr3,
+                   atri_lolim_dr2, atri_uplim_dr2,
+                   atri_lolim_dr3, atri_uplim_dr3,
+                   bj_MAP_dr2, bj_pdf_dr2, bj_distrange_dr2,
+                   bj_MAP_dr3, bj_pdf_dr3, bj_distrange_dr3,
+                   bj_lolim_dr2, bj_uplim_dr2,
+                   bj_lolim_dr3, bj_uplim_dr3,
+                   distxrange = [0.1,10],
+                   keys = ['designation','parallax','parallax_error','parallax_over_error','zpcorr_val','parallax_zpcorr','pmra','pmra_error','pmdec','pmdec_error']):
+    
+    c = vstack([dr2[keys],dr3[keys]])
+
+    fig = plt.figure(figsize=(14,6))
+    ax0 = plt.subplot2grid((3,2),(0,1),fig=fig)
+    ax0.set_title(name, fontsize=20)
+    ax0.errorbar(x = c[0]['parallax'], xerr = c[0]['parallax_error'], y = ['DR2'], 
+                 fmt='x',ms = 10,color=colorset[4],elinewidth=1.9,capsize=3,label='w/o ZP corr')
+    ax0.errorbar(x = c[1]['parallax'], xerr = c[1]['parallax_error'], y = ['DR3'], 
+                 fmt='x',ms = 10,color=colorset[9],elinewidth=1.9,capsize=3)
+    ax0.errorbar(x = c[0]['parallax_zpcorr'], xerr = c[0]['parallax_error'],y = 0.1,
+                 fmt='.',ms = 10,color=colorset[4],elinewidth=1.9,capsize=3,label='w/ ZP corr')
+    ax0.errorbar(x = c[1]['parallax_zpcorr'], xerr = c[1]['parallax_error'],y = 1.1,
+                 fmt='.',ms = 10,color=colorset[9],elinewidth=1.9,capsize=3)
+    ax0.set_xlabel('Parallax (mas)',fontsize=16)
+    ax0.set_ylim(-0.5,1.5)
+    ax0.legend(fontsize=16,bbox_to_anchor=(1,1))
+    ax0.tick_params(axis='both', which='major', labelsize=16)
+    ax0.tick_params(axis='both', which='major', length=5)
+    ax0.tick_params(axis='both', which='minor', length=2.5)
+    ax0.tick_params(axis='both', which='both',direction='in',right=True,top=True)
+    
+    
+    ax1 = plt.subplot2grid((3,2),(1,1),fig=fig)
+    ax1.errorbar(x = c[0]['pmra'], xerr = c[0]['pmra_error'], y = ['DR2'], 
+                 fmt='.',ms = 10,color=colorset[4],elinewidth=1.9,capsize=3)
+    ax1.errorbar(x = c[1]['pmra'], xerr = c[1]['pmra_error'], y = ['DR3'], 
+                 fmt='.',ms = 10,color=colorset[9],elinewidth=1.9,capsize=3)
+    ax1.set_xlabel(r'$\mu_\alpha$ (mas yr$^{-1}$)',fontsize=16)
+    ax1.set_ylim(-0.5,1.5)
+    ax1.tick_params(axis='both', which='major', labelsize=16)
+    ax1.tick_params(axis='both', which='major', length=5)
+    ax1.tick_params(axis='both', which='minor', length=2.5)
+    ax1.tick_params(axis='both', which='both',direction='in',right=True,top=True)
+    
+    ax2 = plt.subplot2grid((3,2),(2,1),fig=fig)
+    ax2.errorbar(x = c[0]['pmdec'], xerr = c[0]['pmdec_error'], y = ['DR2'], 
+                 fmt='.',ms = 10,color=colorset[4],elinewidth=1.9,capsize=3)
+    ax2.errorbar(x = c[1]['pmdec'], xerr = c[1]['pmdec_error'], y = ['DR3'], 
+                 fmt='.',ms = 10,color=colorset[9],elinewidth=1.9,capsize=3)
+    ax2.set_xlabel(r'$\mu_\delta$ (mas yr$^{-1}$)',fontsize=16)
+    ax2.set_ylim(-0.5,1.5)
+    ax2.tick_params(axis='both', which='major', labelsize=16)
+    ax2.tick_params(axis='both', which='major', length=5)
+    ax2.tick_params(axis='both', which='minor', length=2.5)
+    ax2.tick_params(axis='both', which='both',direction='in',right=True,top=True)
+    
+    ax3 = plt.subplot2grid((3,2),(0,0),fig=fig, rowspan=3)
+    ax3.plot(distrange_dr2,atri_pdf_dr2(distrange_dr2),color=colorset[4],label='DR2 (Atri)')
+    ax3.plot(bj_distrange_dr2,bj_pdf_dr2,'--',color=colorset[4],label='DR2 (Bailer-Jones)',lw=3,alpha=0.2)
+    ax3.plot(distrange_dr3,atri_pdf_dr3(distrange_dr3),color=colorset[9],label='EDR3 (Atri)')
+    ax3.plot(bj_distrange_dr3,bj_pdf_dr3,'--',color=colorset[9],label='EDR3 (Bailer-Jones)',lw=3,alpha=0.2)
+    ax3.legend(fontsize=16)
+    ax3.set_xlim(distxrange)
+    ax3.set_ylim(0,)
+    ax3.set_title(f'DR2: $d$(Atri)={atri_MAP_dr2:.2}(-{atri_MAP_dr2-atri_lolim_dr2[0]:.2}/+{atri_uplim_dr2[0]-atri_MAP_dr2:.2}) kpc' +
+                  f'; $d$(BJ)={bj_MAP_dr2:.2}(-{bj_MAP_dr2-bj_lolim_dr2:.2}/+{bj_uplim_dr2-bj_MAP_dr2:.2}) kpc\n' +
+                  f'EDR3: $d$(Atri)={atri_MAP_dr3:.2}(-{atri_MAP_dr3-atri_lolim_dr3[0]:.2}/+{atri_uplim_dr3[0]-atri_MAP_dr3:.2}) kpc' + 
+                  f'; $d$(BJ)={bj_MAP_dr3:.2}(-{bj_MAP_dr3-bj_lolim_dr3:.2}/+{bj_uplim_dr3-bj_MAP_dr3:.2}) kpc', fontsize=16)
+    ax3.set_xlabel(r'Distance (kpc)',fontsize=16)
+    ax3.set_ylabel(r'PDF (kpc$^{-1}$)',fontsize=16)
+    ax3.minorticks_on()
+    ax3.tick_params(axis='both', which='major', labelsize=16)
+    ax3.tick_params(axis='both', which='major', length=9)
+    ax3.tick_params(axis='both', which='minor', length=4.5)
+    ax3.tick_params(axis='both', which='both',direction='in',right=True,top=True)
+    fig.subplots_adjust(hspace=0.6, wspace=0.11)
+    return c, fig
+
+def gaiaedr3_plots(src_name, search_rad, tau_dr2, tau_dr3, distance_plotting_range, distance_method):
+    source_name = src_name
+    gaia_tab_dr2, gaia_tab_dr3 = gaia_search(target_resolver(source_name), search_rad)
+    gaia_zpcorr(gaia_tab_dr2,'dr2');
+    gaia_zpcorr(gaia_tab_dr3,'dr3');
+    if len(gaia_tab_dr2) != 1 and len(gaia_tab_dr2) != 1:
+        print('Error, number of Gaia counterparts found != 1. Change the search radius.')
+        return
+    if distance_method == 'both':
+        atri_MAP_dr2, atri_pdf_dr2, distrange_dr2 = atri_dist(gaia_tab_dr2['parallax_zpcorr'][0],
+                                                              gaia_tab_dr2['parallax_error'][0],
+                                                              gaia_tab_dr2['ra'][0],gaia_tab_dr2['dec'][0])
+
+        atri_MAP_dr3, atri_pdf_dr3, distrange_dr3 = atri_dist(gaia_tab_dr3['parallax_zpcorr'][0],
+                                                              gaia_tab_dr3['parallax_error'][0],
+                                                              gaia_tab_dr3['ra'][0],gaia_tab_dr3['dec'][0])
+
+
+        atri_lolim_dr2, atri_uplim_dr2, ci_dr2 = atri_CIcalc(tau_dr2,atri_MAP_dr2, atri_pdf_dr2,'dr2')
+        atri_lolim_dr3, atri_uplim_dr3, ci_dr3 = atri_CIcalc(tau_dr3,atri_MAP_dr3, atri_pdf_dr3,'dr3')
+
+        bj_pdf_dr2 ,bj_pointest_dr2 = bailerjones(gaia_tab_dr2['ra'][0],gaia_tab_dr2['dec'][0],
+                                                  gaia_tab_dr2['parallax_zpcorr'][0],
+                                                  gaia_tab_dr2['parallax_error'][0])
+
+        bj_pdf_dr3 ,bj_pointest_dr3 = bailerjones(gaia_tab_dr3['ra'][0],gaia_tab_dr3['dec'][0],
+                                                  gaia_tab_dr3['parallax_zpcorr'][0],
+                                                  gaia_tab_dr3['parallax_error'][0])
+
+
+        tab, fig = comparison_all(source_name,gaia_tab_dr2,gaia_tab_dr3,
+                                  atri_MAP_dr2, atri_pdf_dr2, distrange_dr2,
+                                  atri_MAP_dr3, atri_pdf_dr3, distrange_dr3,
+                                  atri_lolim_dr2, atri_uplim_dr2,
+                                  atri_lolim_dr3, atri_uplim_dr3, 
+                                  bj_pointest_dr2['point_est'][0],bj_pdf_dr2['pdf'].data,bj_pdf_dr2['d'].data,
+                                  bj_pointest_dr3['point_est'][0],bj_pdf_dr3['pdf'].data,bj_pdf_dr3['d'].data,
+                                  bj_pointest_dr2['point_est'][1],bj_pointest_dr2['point_est'][2],
+                                  bj_pointest_dr3['point_est'][1],bj_pointest_dr3['point_est'][2],
+                                  distxrange=distance_plotting_range);
+    elif distance_method == 'atri':
+        atri_MAP_dr2, atri_pdf_dr2, distrange_dr2 = atri_dist(gaia_tab_dr2['parallax_zpcorr'][0],
+                                                              gaia_tab_dr2['parallax_error'][0],
+                                                              gaia_tab_dr2['ra'][0],gaia_tab_dr2['dec'][0])
+
+        atri_MAP_dr3, atri_pdf_dr3, distrange_dr3 = atri_dist(gaia_tab_dr3['parallax_zpcorr'][0],
+                                                              gaia_tab_dr3['parallax_error'][0],
+                                                              gaia_tab_dr3['ra'][0],gaia_tab_dr3['dec'][0])
+
+
+        atri_lolim_dr2, atri_uplim_dr2, ci_dr2 = atri_CIcalc(tau_dr2,atri_MAP_dr2, atri_pdf_dr2,'dr2')
+        atri_lolim_dr3, atri_uplim_dr3, ci_dr3 = atri_CIcalc(tau_dr3,atri_MAP_dr3, atri_pdf_dr3,'dr3')
+
+        tab, fig = comparison(source_name,gaia_tab_dr2,gaia_tab_dr3, 'Atri',
+                              atri_MAP_dr2, atri_pdf_dr2, distrange_dr2,
+                              atri_MAP_dr3, atri_pdf_dr3, distrange_dr3,
+                              atri_lolim_dr2, atri_uplim_dr2,
+                              atri_lolim_dr3, atri_uplim_dr3,
+                              distxrange=distance_plotting_range);
+    elif distance_method == 'bj':
+        bj_pdf_dr2 ,bj_pointest_dr2 = bailerjones(gaia_tab_dr2['ra'][0],gaia_tab_dr2['dec'][0],
+                                                  gaia_tab_dr2['parallax_zpcorr'][0],
+                                                  gaia_tab_dr2['parallax_error'][0])
+
+        bj_pdf_dr3 ,bj_pointest_dr3 = bailerjones(gaia_tab_dr3['ra'][0],gaia_tab_dr3['dec'][0],
+                                                  gaia_tab_dr3['parallax_zpcorr'][0],
+                                                  gaia_tab_dr3['parallax_error'][0])
+
+        tab, fig = comparison(source_name,gaia_tab_dr2,gaia_tab_dr3, 'BJ',
+                              bj_pointest_dr2['point_est'][0],bj_pdf_dr2['pdf'].data,bj_pdf_dr2['d'].data,
+                              bj_pointest_dr3['point_est'][0],bj_pdf_dr3['pdf'].data,bj_pdf_dr3['d'].data,
+                              bj_pointest_dr2['point_est'][1],bj_pointest_dr2['point_est'][2],
+                              bj_pointest_dr3['point_est'][1],bj_pointest_dr3['point_est'][2],
+                              distxrange=distance_plotting_range);
+
+    return tab, fig
+
